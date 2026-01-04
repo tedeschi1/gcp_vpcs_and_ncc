ce3fd72a69838169f5ea99142f0eefdcf6927273
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-04 14:17:34 -0600

    Add GCP variables for project, region, and credentials

---
+++ gcp-vnets/variables.tf
@@ -0,0 +1,14 @@
+variable "gcp_project_id" {
+  type = string
+  default = "single-quanta-461820-s0"
+}
+
+variable "gcp_region" {
+  type = string
+  default = "us-central1"
+}
+
+variable "gcp_credentials_file" {
+  type = string
+  default = "C:/Users/Michael Tedeschi/Downloads/csp-gcp-mtedeschi-b944a061f93c.json"
+}
