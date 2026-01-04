ba54948d918540099525da933bbfaa1c798607bb
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-04 15:59:30 -0600

    Add VPC east name variable to Terraform config

--- gcp-vnets/variables.tf
+++ gcp-vnets/variables.tf
@@ -12,3 +12,8 @@ variable "gcp_credentials_file" {
   type = string
   default = "C:/Users/Michael Tedeschi/Downloads/csp-gcp-mtedeschi-b944a061f93c.json"
 }
+
+variable "vpc_east_name" {
+  type        = string
+  description = "The name of the VPC in the east region"
+}
