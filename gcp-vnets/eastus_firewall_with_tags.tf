0ae51ea08985eb0d05357080c3d3716810176bc9
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-05 12:07:49 -0600

    add firewall rules

---
+++ gcp-vnets/eastus_firewall_with_tags.tf
@@ -0,0 +1,28 @@
+resource "google_compute_firewall" "vpc1_east1_rules" {
+  project     = var.gcp_project_id
+  name        = "us-east1-firewall-rules"
+  network     = "vpc1-east1"
+  description = "Creates firewall rule targeting tagged instances"
+  direction   = "INGRESS"
+  priority    = 1000
+
+  #Allow in-browser SSH
+  source_ranges = ["35.235.240.0/20", "10.0.0.0/21"]
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["22"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  target_tags = ["allow-ssh"]
+
+  log_config {
+    metadata = "INCLUDE_ALL_METADATA"
+  }
+
+  depends_on = [google_compute_network.vpc1_east1]
+}
