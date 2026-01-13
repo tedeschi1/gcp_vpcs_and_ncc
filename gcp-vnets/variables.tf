c6258991f1b9a78234b27788aa0d34cc57951214
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-13 12:25:58 -0600

    added variable descriptions

--- gcp-vnets/variables.tf
+++ gcp-vnets/variables.tf
@@ -13,11 +13,13 @@ variable "vpc_east_name" {
   description = "The name of the VPC in the east region"
 }
 
+#Used in south1 compute insance configuration
 variable "instance_names" {
   type    = list(string)
   default = ["web-server", "app-server", "db-server"]
 }
 
+#Used in south1 sn2 compute instance configuration
 variable "server_config" {
   default = {
     "web-prod" = { type = "n2-standard-2", zone = "us-south1-b" }
